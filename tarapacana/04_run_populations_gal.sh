7932234718b46ea322b6ff54661b6d4c6460e28e
Madeline G. Eppley <90652097+madeline-eppley@users.noreply.github.com>
2026-02-04 14:02:43 -0500

    Update 04_run_populations_gal.sh

--- tarapacana/04_run_populations_gal.sh
+++ tarapacana/04_run_populations_gal.sh
@@ -1,7 +1,6 @@
 (base) [eppley.m@explorer-01 tarapacana]$ cat run_populations_gal.sh
 #!/bin/bash
-
-#SBATCH --partition=lotterhos
+#SBATCH --partition=short
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
@@ -13,7 +12,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=eppley.m@northeastern.edu
 
-export PATH=/projects/gatins/programs_explorer/stacks_2.68/bin:$PATH #globally avail on explorer
+export PATH=/projects/gatins/programs_explorer/stacks_2.68/bin:$PATH # globally available on explorer
 
 BASE_DIR="/projects/gatins/2025_Mobulid/tarapacana"
 STACKS_DIR="${BASE_DIR}/final_m3_M2_n2"
@@ -22,7 +21,7 @@ POPMAP="${BASE_DIR}/popmap_gal.txt"
 
 mkdir -p ${OUTDIR}
 
-echo "~~~ Running populations with new 4 pop (EAST, Central, West, Galapagos/GAL) popmap ~~~"
+echo "~~~ Running populations with new 4 pop (EAST, GAL, CTR, WST) popmap ~~~"
 echo "Input: ${STACKS_DIR}"
 echo "Output: ${OUTDIR}"
 echo "Popmap:"
@@ -32,9 +31,44 @@ populations -P ${STACKS_DIR} \
   -M ${POPMAP} \
   -O ${OUTDIR} \
   -r 0.8 \
+  -p 2 \
   --min-maf 0.05 \
+  --write-single-snp \
   --vcf \
   --fstats \
   -t 8
+
+echo "~~~ Populations complete, starting VCFtools filtering ~~~"
+
+module load vcftools
+
+INPUT_VCF="${OUTDIR}/populations.snps.vcf"
+
+# minDP 10
+vcftools --vcf ${INPUT_VCF} --minDP 10 --recode --recode-INFO-all --out ${OUTDIR}/minDP10
+
+# max-missing 0.8
+vcftools --vcf ${OUTDIR}/minDP10.recode.vcf --max-missing 0.8 --recode --recode-INFO-all --out ${OUTDIR}/minDP10_maxmiss0.8
+
+# check missingness per ind
+vcftools --vcf ${OUTDIR}/minDP10_maxmiss0.8.recode.vcf --missing-indv --out ${OUTDIR}/missingness
+
+echo "~~~ Individual missingness ~~~"
+cat ${OUTDIR}/missingness.imiss
+
+# remove individuals with >40% missing
+awk '$5 > 0.4 {print $1}' ${OUTDIR}/missingness.imiss > ${OUTDIR}/remove_individuals.txt
+
+if [ -s ${OUTDIR}/remove_individuals.txt ]; then
+    echo "Removing individuals with >40% missing data:"
+    cat ${OUTDIR}/remove_individuals.txt
+    vcftools --vcf ${OUTDIR}/minDP10_maxmiss0.8.recode.vcf --remove ${OUTDIR}/remove_individuals.txt --recode --recode-INFO-all --out ${OUTDIR}/tarapacana_4pop_final
+else
+    echo "No individuals to remove, copying final VCF"
+    cp ${OUTDIR}/minDP10_maxmiss0.8.recode.vcf ${OUTDIR}/tarapacana_4pop_final.recode.vcf
+fi
+
+echo "final SNP count"
+grep -v "^#" ${OUTDIR}/tarapacana_4pop_final.recode.vcf | wc -l
 
 ls -la ${OUTDIR}
